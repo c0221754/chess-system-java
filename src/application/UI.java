d0a4b4c52e250a0c86b8ebcd394389a30a8eeb15
Hugo Souza <hugo.souza@accenture.com>
2019-03-29 16:49:16 -0300

    Placing pieces on the board

--- src/application/UI.java
+++ src/application/UI.java
@@ -7,7 +7,7 @@ public class UI {
 	public static void printBoard(ChessPiece[][] pieces) {
 		for (int i=0; i < pieces.length; i++) {
 			System.out.print((8 - i) + " ");
-			//Considerando uma matrix quadrada.
+			//Considerando uma matriz quadrada.
 			for(int j=0; j < pieces.length; j++) {
 				printPiece(pieces[i][j]);
 			}
